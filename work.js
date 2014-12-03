997c5abf94c80e1d10a66cdc2936eb4f118b89d2
Brett <brettgurman@gmail.com>
2014-12-03 18:48:51 -0500

    Edited work.html

---
+++ work.js
@@ -0,0 +1,42 @@
+// $(document).ready(function(){
+//         $("#requestButton").click(function(){
+//                 $("#requestButton").hide()
+//                 $("#requestForm").show()
+//         })
+// })
+
+$(document).ready(function(){
+        $("#comp40title").click(function(){
+                $("#comp40content").fadeToggle("fast");
+        });
+});
+
+$(document).ready(function(){
+        $("#comp105title").click(function(){
+                $("#comp105content").fadeToggle("fast");
+        });
+});
+
+$(document).ready(function(){
+        $("#futureTitle").click(function(){
+                $("#futureContent").fadeToggle("fast");
+        });
+});
+
+// $(document).ready(function(){
+//         $("#requestSubmit").click(function(){
+//                 $("#requestForm").hide();
+//                 var email = $("#email").text();
+//                 var github = $("#github").text();
+//                 var body = $("#email_body").text();
+//                 window.open('mailto:brettgurman@gmail.com?subject=github&body=body');
+//                 window.close();
+//         });
+// });
+
+
+
+
+
+
+
