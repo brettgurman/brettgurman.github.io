42de6939408df423c3eebc870dcbcdd822e79ad3
Brett Gurman <brettgurman@gmail.com>
2015-10-24 17:13:01 -0400

    edited work.js

--- work.js
+++ work.js
@@ -11,6 +11,24 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+        $("#comp150title").click(function(){
+                $("#comp150content").fadeToggle("fast");
+        });
+});
+
+$(document).ready(function(){
+        $("#comp170title").click(function(){
+                $("#comp170content").fadeToggle("fast");
+        });
+});
+
+$(document).ready(function(){
+        $("#comp175title").click(function(){
+                $("#comp175content").fadeToggle("fast");
+        });
+});
+
+$(document).ready(function(){
         $("#futureTitle").click(function(){
                 $("#futureContent").fadeToggle("fast");
         });
